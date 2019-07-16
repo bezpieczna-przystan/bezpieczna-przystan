57d8ab4e8364d3e07a293dedd9d90138ad1912d7
voycik <voycik@gmail.com>
2019-07-16 17:32:16 +0200

    Add invisible captcha to all contact forms

--- app/controllers/adoptions_controller.rb
+++ app/controllers/adoptions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class AdoptionsController < ApplicationController
+  invisible_captcha only: %i[create send_preferences], honeypot: :subtitle
   before_action :find_animal, only: %i[new create]
   def new
     @adoption = DogAdoption.new
